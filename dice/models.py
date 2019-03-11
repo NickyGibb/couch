5e97f806653cb9d6b5a10310543e858d0098512f
nicholasmgibb@gmail.com <nicholasmgibb@gmail.com>
2019-03-11 15:58:21 +0000

    update database

--- dice/models.py
+++ dice/models.py
@@ -3,14 +3,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-class Category(models.Model):
-    name = models.CharField(max_length=128,unique=True)
 
-    def __str__(self):
-        return self.name
 
 class User(models.Model):
-    category = models.ForeignKey(Category)
+
     user_name = models.CharField(max_length=128, unique=False)
     user_email = models.CharField(max_length=128, unique=True)
     #score = models.FloatField(_(u"Score"), choices= SCORE_CHOICES, default=3.0)
@@ -20,7 +16,7 @@ class User(models.Model):
         return self.user_name
 
 class Game(models.Model):
-    category = models.ForeignKey(Category)
+
     game_name = models.CharField(max_length=128, unique =True)
     game_type = models.CharField(max_length=128, unique= False)
     game_genre = models.CharField(max_length=128, unique=False)
@@ -31,10 +27,10 @@ class Game(models.Model):
         return self.game_name
 
 class Forum(models.Model):
-    category = models.ForeignKey(Category)
+
     post_title = models.CharField(max_length=128, unique = False)
     post_user = models.ForeignKey(User)
-    #post_date = models.DateTimeField(default=datetime.now(), blank=True)
+
 
 
     def __str__(self):

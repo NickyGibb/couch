24bd620ffa58f6e474d65324268c4172e9b92ae6
nicholasmgibb@gmail.com <nicholasmgibb@gmail.com>
2019-03-12 21:48:55 +0000

    db

--- dice/models.py
+++ dice/models.py
@@ -3,13 +3,25 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
 
 
 class User(models.Model):
 
     user_name = models.CharField(max_length=128, unique=False)
     user_email = models.CharField(max_length=128, unique=True)
-    #score = models.FloatField(_(u"Score"), choices= SCORE_CHOICES, default=3.0)
+    user_views = models.IntegerField(default=0)
+    user_endorsements = models.IntegerField(default=0)
+    user_image = models.ImageField(upload_to= 'user_image', blank=True)
+
 
 
     def __str__(self):
@@ -20,8 +32,9 @@ class Game(models.Model):
     game_name = models.CharField(max_length=128, unique =True)
     game_type = models.CharField(max_length=128, unique= False)
     game_genre = models.CharField(max_length=128, unique=False)
-    #score = models.FloatField(_(u"Score"), choices= SCORE_CHOICES, default=3.0)
-    player_number = models.IntegerField(max_length= 3,unique = True)
+    player_number = models.IntegerField(default=0)
+    game_views = models.IntegerField(default=0)
+    game_endoresments = models.IntegerField(default=0)
     game_image = models.ImageField(upload_to= 'game_image', blank=True)
 
     def __str__(self):
@@ -29,8 +42,11 @@ class Game(models.Model):
 
 class Forum(models.Model):
 
-    post_title = models.CharField(max_length=128, unique = False)
+    post_title = models.CharField(max_length=128,default="", unique = False)
     post_user = models.ForeignKey(User)
+    post_type = models.CharField(max_length=128,default="", unique= False)
+    post_views = models.IntegerField(default=0)
+    post_endorsements = models.IntegerField(default=0)
 
 
 

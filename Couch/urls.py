25111aaf09ffa5b6037ab4c29a35789c3755fb2b
nicholasmgibb@gmail.com <nicholasmgibb@gmail.com>
2019-03-21 15:09:08 +0000

    view fix

--- Couch/urls.py
+++ Couch/urls.py
@@ -25,25 +25,11 @@ from registration.backends.simple.views import RegistrationView
 
 
 urlpatterns = [
-    url(r'^dice/home^$', views.home, name = 'home'),
-    url(r'^dice/about/$', views.about, name = 'about'),
-    url(r'^dice/category/(?P<category_name_slug>[\w\-]+)/$', views.show_category, name = 'show_category'),
+    url(r'^$', views.home, name = 'home'),
+    url(r'^about', views.about, name = 'about'),
     url(r'^dice/', include('dice.urls')),
-    url(r'^dice/user/$',views.user, name = 'user'),
-    url(r'^dice/game/$', views.game, name = 'game'),
-    url(r'^dice/forum/', include(board.urls)),
-    url(r'^dice/login/$', auth_views.login, {'template_name': 'dice/login.html'}, name='login'),
-    url(r'^dice/logout/$', auth_views.logout, {'template_name': 'dice/logged_out.html'}, name='logout'),
-    url(r'^dice/admin/', admin.site.urls),
-    url(r'^dice/register/$', views.register, name='register'),
-    url(r'^dice/password/$', views.change_password, name='change_password'),
-    url(r'^dice/register_profile/$', views.register_profile, name='register_profile'),
-    url(r'^dice/profile/(?P<user_name>[\w\-]+)/$', views.profile, name='profile'),
-    url(r'^dice/events/$', views.events, name='events'),
-    url(r'^dice/createevent/$', views.create_event, name='create_event'),
-    url(r'^accounts/', include('registration.backends.simple.urls')),
-    url(r'^like/$', views.like_game, name='like_game'),
-    url(r'^dislike/$', views.dislike_game, name='dislike_game'),
+    url(r'^admin/', admin.site.urls),
+    
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

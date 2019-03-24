1e4def857bd17a30d5ad93ce876a94c5093c4a75
rdmaloney <2026124M@student.gla.ac.uk>
2019-03-24 13:37:52 +0000

    getting rid of mezzine

--- dice/models.py
+++ dice/models.py
@@ -6,7 +6,7 @@ from django.db import models
 from django import forms
 from django.db.models.signals import post_save
 from django.utils.translation import ugettext_lazy as _
-from mezzanine.core.fields import FileField
+
 
 
 class Category(models.Model):
